f11b1b4b4f8050b435bac669f640ac53d3f07be5
firumon <github@firumon.com>
2018-09-14 19:18:30 +0530

    Removed title properties to pagetitle module

--- assets/js/vuex/modules/page.js
+++ assets/js/vuex/modules/page.js
@@ -8,7 +8,6 @@ const actions = {
 const mutations = {
     requestInterceptor(state){ state.loading = true; },
     responseInterceptor(state){ state.loading = false; },
-    title(state,payload){ state.title = _.isObject(payload) ? payload.title : payload; },
     loading(state,payload){ state.loading = _.isObject(payload) ? (payload.loading || payload.status || payload.load) : payload; },
 };
 
