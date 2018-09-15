a541aaa6f225163d695207eb3bc56468eb434823
firumon <github@firumon.com>
2018-09-15 19:27:56 +0530

    Removed commented line

--- assets/js/components.js
+++ assets/js/components.js
@@ -4,5 +4,4 @@ const requireComponent = require.context('../components',true,/\.vue$/);
 requireComponent.keys().forEach(Name => {
     let Options = requireComponent(Name);
     Vue.component(Name.replace(/^\.\/(_\w+\/)*(.*)\.\w+$/, '$2'),Options.default || Options);
-    //console.log(Name,Name.replace(/^\.\/(_\w+\/)*(.*)\.\w+$/, '$2'))
 });
