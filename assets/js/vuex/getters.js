5f22171b17629431da1d09c8c4fd6415a06e37fb
firumon <github@firumon.com>
2018-09-16 09:19:19 +0530

    Removed actions and menu which is no longer in use

--- assets/js/vuex/getters.js
+++ assets/js/vuex/getters.js
@@ -16,9 +16,6 @@ export default {
     resources(state,getters){ return _.mapValues(_.keyBy(getters._resources,'resource.id'),'resource.title') },
 
     _actions(state,getters){ return _.flatMap(getters._resources,'actions') },
-    actions(state,getters){ return VX.getKeyedObject(getters._actions,'id',['confirm','on','icon','set','type','title','method','resource']) },
-
-    menu(state,getters){ return _.mapValues(_.groupBy(getters._actions,'resource'),function(_ary){ return VX.getKeyedObject(_ary,'id',['menu','id']) }) },
 
     resolution(state,getters){ return (action) => getters['RSLV/resolution'](action) },
     contentAction(state,getters){ return (content) => getters['CONT/action'](content) },
