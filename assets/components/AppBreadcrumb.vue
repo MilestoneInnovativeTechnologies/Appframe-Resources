aa4162c00878e67972357289a97e823bd0f95b41
firumon <github@firumon.com>
2018-09-16 17:25:10 +0530

    Breadcrumb implemented

--- assets/components/AppBreadcrumb.vue
+++ assets/components/AppBreadcrumb.vue
@@ -3,14 +3,14 @@
         <ol class="breadcrumb">
             <li class="breadcrumb-item active" v-for="(route,index) in list">
                 <a v-if="!index" href="#"><icon class="breadcrumb-icon mr-2">angle-left</icon>{{ route }}</a>
-                <a v-else @click.prevent="navigate(route)" href="#">{{ compress(route.title) }}</a>
+                <a v-else @click.prevent="go(route)" href="#">{{ compress(route.title) }}</a>
             </li>
         </ol>
     </nav>
 </template>
 
 <script>
-    import { mapGetters } from'vuex';
+    import { mapGetters,mapActions } from'vuex';
     export default {
         name: "AppBreadcrumb",
         data(){ return { max:20,threshold:20 } },
@@ -19,8 +19,9 @@
             list(){ return this.set() }
         },
         methods: {
-            navigate(route){ console.log('Navigate to: ',route) },
-            compress(text){ if(!text) return ''; console.log(this.max/2-3,this.max/2+3);
+            ...mapActions(['navigate']),
+            go({ name,params }){ this.navigate({ name,params }) },
+            compress(text){ if(!text) return '';
                 return (text.length > this.max * (1+this.threshold/100))
                     ? [ text.substr(0,this.max/2-3),text.substr(text.length-this.max/2+3) ].join('.....')
                     : text;
