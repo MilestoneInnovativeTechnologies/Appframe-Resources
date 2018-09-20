61b56a8e24e2947d4e8e54f54b98c5dee05d8a99
firumon <github@firumon.com>
2018-09-20 20:50:29 +0530

    Optimized HTML

--- assets/components/_list/BSList.vue
+++ assets/components/_list/BSList.vue
@@ -1,18 +1,12 @@
 <template>
-    <div>
-        <div class="card">
-            <div class="card-body">
-                <form>
-                    <div class="table-responsive">
-                        <table class="table table-sm mb-0 table-hover">
-                            <BSListTHead :columns="Object.keys(layout)" :data-list-id="dataListId"></BSListTHead>
-                            <BSListTBody :columns="Object.values(layout)" :data="data" :data-list-id="dataListId"></BSListTBody>
-                        </table>
-                    </div>
-                </form>
-            </div>
+    <div class="card"><div class="card-body"><form>
+        <div class="table-responsive">
+            <table class="table table-sm mb-0 table-hover">
+                <BSListTHead :columns="Object.keys(layout)" :data-list-id="dataListId"></BSListTHead>
+                <BSListTBody :columns="Object.values(layout)" :data="data" :data-list-id="dataListId"></BSListTBody>
+            </table>
         </div>
-    </div>
+    </form></div></div>
 </template>
 
 <script>
