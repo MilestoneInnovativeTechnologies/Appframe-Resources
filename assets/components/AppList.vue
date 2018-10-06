c525d80714937a58957608b7af23b5fa6a42fa93
Firose Hussain <github@firumon.com>
2018-10-06 20:18:08 +0530

    Modified for displaying pagination

--- assets/components/AppList.vue
+++ assets/components/AppList.vue
@@ -6,25 +6,31 @@
 </template>
 
 <script>
-    import { createNamespacedHelpers } from 'vuex';
-    const { mapActions,mapGetters } = createNamespacedHelpers('LIST');
+    import { mapActions,mapGetters,mapState } from 'vuex';
     export default {
         name: "AppList",
         props: ['dataIds'],
         mixins: [require('./../js/common/ListDefaultLayoutMixin').listDefaultLayoutMixin],
         computed: {
             id(){ return this.dataIds['idn1'] },
-            ...mapGetters({ getList:'list',getLayout:'layout' }),
-            data(){ return this.getList(this.id) },
+            ...mapGetters('LIST',{ getList:'list',getLayout:'layout' }), ...mapGetters('PGNT',{ getDetails:'list',getItems:'items' }), ...mapState('PGNT',[ 'page' ]),
+            list(){ return this.getList(this.id) }, items(){ return this.getItems(this.id) },
+            pagination(){ return { ...(this.getDetails(this.id)),page:this.page } },
+            fetchable(){ return _.difference(_.map(this.items,_.toString),_.keys(this.list)) },
+            data(){ return _.pick(this.list,this.items) },
             layout(){ return this.getLayout(this.id) || this.defaultLayout(this.data) },
             action(){ return this.$route.params.action },
         },
         methods: {
-            ...mapActions({ updateList: 'update' }),
+            ...mapActions('LIST',{ updateList: 'update',post: 'post' }),
         },
         created(){
             this.updateList(this.action);
         },
+        watch: {
+            page(page){ this.post({ action:this.action,page }) },
+            fetchable(get){ if(!_.isEmpty(get)) this.post({ action:this.action,get }) }
+        }
     }
 </script>
 
