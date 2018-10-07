56fbd8f9491ff8012985a1171a4c45684de42aa8
Firose Hussain <github@firumon.com>
2018-10-07 19:21:07 +0530

    No more in use

--- assets/js/common/ListDefaultLayoutMixin.js
+++
@@ -1,9 +0,0 @@
-export const listDefaultLayoutMixin = {
-
-    methods: {
-        defaultLayout(lists){
-            return _(_.head(_.values(lists))).pickBy((val) => !(_.isArray(val) || _.isObject())).keys().mapKeys((field) => _.startCase(_.replace(field,/(\W|_)/g,' '))).mapValues((field) => _.zipObject(['field','path'],[field,''])).value();
-        }
-    }
-
-}
