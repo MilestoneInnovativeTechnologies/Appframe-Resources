55348642168f140f64460d28b4666f18df4bf082
Firose Hussain <github@firumon.com>
2018-10-14 20:09:53 +0530

    VueGraph and its components added

--- assets/js/components.js
+++ assets/js/components.js
@@ -5,3 +5,11 @@ requireComponent.keys().forEach(Name => {
     let Options = requireComponent(Name);
     Vue.component(Name.replace(/^\.\/(_\w+\/)*(.*)\.\w+$/, '$2'),Options.default || Options);
 });
+
+import GraphLine3D from 'vue-graph/src/components/line3d.js'
+import NoteWidget from 'vue-graph/src/widgets/note.js'
+import LegendWidget from 'vue-graph/src/widgets/legends.js'
+
+Vue.component(GraphLine3D.name, GraphLine3D);
+Vue.component(NoteWidget.name, NoteWidget);
+Vue.component(LegendWidget.name, LegendWidget);
--- assets/js/framework.js
+++ assets/js/framework.js
@@ -2,5 +2,6 @@
 global.Vue = require('vue');
 global.Vuex = require('vuex');
 global.VueRouter = require('vue-router').default;
+import VueGraph from 'vue-graph'
 
-Vue.use(Vuex,VueRouter);
+Vue.use(Vuex,VueRouter,VueGraph);
