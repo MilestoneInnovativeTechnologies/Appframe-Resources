0af3649d9c1b65d13ef66c85dcd6b14e22566401
Firose Hussain <github@firumon.com>
2018-10-16 05:02:19 +0530

    VueGraph components are added

--- assets/js/components.js
+++ assets/js/components.js
@@ -6,10 +6,14 @@ requireComponent.keys().forEach(Name => {
     Vue.component(Name.replace(/^\.\/(_\w+\/)*(.*)\.\w+$/, '$2'),Options.default || Options);
 });
 
-import GraphLine3D from 'vue-graph/src/components/line3d.js'
-import NoteWidget from 'vue-graph/src/widgets/note.js'
-import LegendWidget from 'vue-graph/src/widgets/legends.js'
+const vueGraphWidgets = ['note','legends','tooltip','guideline'];
+vueGraphWidgets.forEach(Name => {
+    let Options = require('vue-graph/src/widgets/' + Name + '.js');
+    Vue.component(Options.default.name,Options.default || Options);
+});
 
-Vue.component(GraphLine3D.name, GraphLine3D);
-Vue.component(NoteWidget.name, NoteWidget);
-Vue.component(LegendWidget.name, LegendWidget);
+const vueGraphs = ['line-dateblock'];
+vueGraphs.forEach(Name => {
+    let Options = require('vue-graph/src/components/' + Name + '.js');
+    Vue.component(Options.default.name,Options.default || Options);
+});
