b69c78e0984b6b4507012ce1f2f498206afac197
Firose Hussain <github@firumon.com>
2018-10-16 04:58:36 +0530

    Dashboard Components created

---
+++ assets/components/AppDashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="dashboard">
+        <DashboardSection  v-for="section in dashboard.sections" :id="section" :dashboard="id" :key="['DBRD',id,'SCT',section].join('-')"></DashboardSection>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    export default {
+        name: "AppDashboard",
+        props: ['dataIds'],
+        computed: {
+            id(){ return this.dataIds['idn1'] },
+            ...mapGetters('DBRD',{ getDashboard:'dashboard' }),
+            dashboard(){ return this.getDashboard(this.id) }
+        },
+    }
+</script>
