b46dcc55aab8df5a8bc79a37cf7b854ef998afa4
Firose Hussain <github@firumon.com>
2018-10-22 13:46:38 +0530

    url updated to start from root

--- assets/js/axios.js
+++ assets/js/axios.js
@@ -11,7 +11,7 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 if (!token) { console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token'); }
 
 const axiosInstance = axios.create({
-    baseURL: _.trim(window.VuexStoreState.root_path,'/') + '/server',
+    baseURL: '/' + _.trim(window.VuexStoreState.root_path,'/') + '/server',
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
         'X-CSRF-TOKEN': token.content
