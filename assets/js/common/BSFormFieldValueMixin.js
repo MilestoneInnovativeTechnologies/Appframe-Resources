db5d1e0a7c6315a6e1ccc06725a8e9dcb7430c70
Firose Hussain <github@firumon.com>
2018-10-27 13:43:30 +0530

    made use of unselect

--- assets/js/common/BSFormFieldValueMixin.js
+++ assets/js/common/BSFormFieldValueMixin.js
@@ -8,11 +8,15 @@ export const fieldValueMixin = {
         value: {
             get: function() { return (this.$store.getters['FORM/value'](this.dataFormId,this.name)) },
             set: function(value) { this.updateValue({ form: this.dataFormId, field: this.name, value }) }
+        },
+        unvalue: {
+            get: function() { return (this.$store.getters['FORM/value'](this.dataFormId,this.name)) },
+            set: function(value) { this.removeValue({ form: this.dataFormId, field: this.name, value }) }
         }
     },
 
     methods: {
-        ...mapMutations('FORM',['updateValue']),
+        ...mapMutations('FORM',['updateValue','removeValue']),
         input(e){ this.updateValue({ form: this.dataFormId, field: this.name, value: e.target.value }) }
     }
 
