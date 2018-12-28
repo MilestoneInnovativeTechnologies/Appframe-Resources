d17bf01145bf16897dbd8570a396be8e7293f6e1
Firose Hussain <github@firumon.com>
2018-12-28 18:13:08 +0530

    ListActionWarning separated

--- components/_list/AppContentListAction.vue
+++ components/_list/AppContentListAction.vue
@@ -1,18 +1,7 @@
 <template>
     <div class="btn-toolbar mt-1 mt-md-0" v-if="visible">
         <ListAction v-for="actn in contentActions" v-bind="actn" :list-id="id" :key="['list',id,'action',actn.id].join('-')" :confirm="getConfirm(actn.id)"></ListAction>
-        <div class="modal modal-alert fade" id="ListWarningModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header"><h5 class="modal-title"><icon class="text-warning mr-1">bullhorn</icon> Confirm </h5></div>
-                    <div class="modal-body"><p class="warning-msg"></p></div>
-                    <div class="modal-footer">
-                        <button type="button" id="ListWarningDismiss" class="btn btn-warning">Proceed</button>
-                        <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <ListActionWarning></ListActionWarning>
     </div>
 </template>
 
---
+++ components/_list/ListActionWarning.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="modal modal-alert fade" id="ListWarningModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header"><h5 class="modal-title"><icon class="text-warning mr-1">bullhorn</icon> Confirm </h5></div>
+                <div class="modal-body"><p class="warning-msg"></p></div>
+                <div class="modal-footer">
+                    <button type="button" id="ListWarningDismiss" class="btn btn-warning">Proceed</button>
+                    <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ListActionWarning"
+    }
+</script>
