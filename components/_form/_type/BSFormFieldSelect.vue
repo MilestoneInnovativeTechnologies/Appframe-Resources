b32759571d558e2f4a3ebde4c31d564f52874d66
Firose Hussain <github@firumon.com>
2018-12-31 15:27:41 +0530

    Fixed issue with enum field selecting default value

--- components/_form/_type/BSFormFieldSelect.vue
+++ components/_form/_type/BSFormFieldSelect.vue
@@ -1,5 +1,5 @@
 <template>
-    <select :name="name" v-model="value" class="custom-select">
+    <select :name="name" v-model="value" class="custom-select" :id="control_id">
         <option v-for="(label,value) in options" :value="value" :key="['SO',dataFormId,name,value].join('-')">{{ label }}</option>
     </select>
 </template>
@@ -8,7 +8,7 @@
     import { mapGetters,mapActions } from 'vuex';
     export default {
         name: "BSFormFieldSelect",
-        props:['dataFormId','name'],
+        props:['dataFormId','name','id'],
         mixins: [require('./../../../js/common/BSFormFieldValueMixin').fieldValueMixin,require('./../../../js/common/BSFormFieldDependMixin').fieldDependMixin],
         computed: {
             ...mapGetters('FORM',{ getOption:'option' }),
@@ -16,24 +16,30 @@
             option(){ return this.getOption(this.dataFormId,this.name); },
             option_id(){ return this.option ? this.option.id : null },
             options(){ return this.getOptions(this.option_id); },
+            control_id(){ return [this.name,this.dataFormId,this.id].join('_') },
         },
         methods: {
             ...mapActions('FOPT',{ fetchOptions:'fetch' }),
             initSelect2(){
                 let vm = this, options = { minimumResultsForSearch: 12, allowClear: true, placeholder: '' },
-                    value = vm.value || ((this.option.type === 'Enum' && this.options) ? _.head(_.keys(this.options)) : null),
-                    select2 = $(`select[name="${vm.name}"]`), S2Width = null;
-                options.value = value;
-                if(select2.hasClass("select2-hidden-accessible")) { S2Width = select2.next().css('width'); try { select2.select2('destroy'); } catch(e){} }
-                select2
-                    .select2(options)
-                    .on('select2:select',function(e){ vm.value = e.params.data.id; })
-                    .on('select2:unselect',function(e){ vm.unvalue = e.params.data.id; });
-                if(S2Width) select2.next().css('width',S2Width);
+                    value = vm.value || ((this.option.type === 'Enum' && this.options) ? _.head(_.keys(this.options)) : (_.has(vm.$attrs,'multiple') ? [] : null)),
+                    select2 = $(`select#${this.control_id}`);
+                if(!select2.hasClass("select2-hidden-accessible")){
+                    select2
+                        .select2(options)
+                        .on('select2:select',function(e){ vm.value = e.params.data.id; })
+                        .on('select2:unselect',function(e){ vm.unvalue = e.params.data.id; });
+                    if(!_.isEmpty(value)) select2.val(value).trigger('change');
+                    //select2.next().css('width','100%');
+                }
             }
         },
         created(){
             if(this.option.preload === 'Yes' && (!this.options || this.option.type !== 'Enum')) this.fetchOptions({ id:this.option_id,latest:this.getLatest(this.option_id) });
+        },
+        beforeDestroy () {
+            let select2 = $(`select#${this.control_id}`);
+            if(select2.hasClass("select2-hidden-accessible")) { try { select2.select2('destroy'); } catch(e){ } }
         },
         mounted(){
             this.initSelect2();
