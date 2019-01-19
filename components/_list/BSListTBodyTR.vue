f16c1794f87b79eeffd781e84acf6497e6abd6ca
Firose Hussain <github@firumon.com>
2019-01-19 22:05:39 +0530

    Arrays wont list, instead any display keys will be mapped and joined with coma(,)

--- components/_list/BSListTBodyTR.vue
+++ components/_list/BSListTBodyTR.vue
@@ -10,11 +10,18 @@
     export default {
         name: "BSListTBodyTR",
         props: ['columns','record','dataListId','dataRecordId'],
+        data(){ return { fetchKeys: ['name','title','label','display','caption','id'] } },
         methods: {
-            data(pathProps){ return (pathProps.path) ? this.dataFromPath(pathProps.path,pathProps.field): _.get(this.record,pathProps.field); },
+            data(pathProps){ return (pathProps.path) ? this.dataFromPath(pathProps.path,pathProps.field): this.tune(_.get(this.record,pathProps.field)); },
             dataFromPath(path,field){ let initData = _.get(this.record,path); return (_.isArray(initData)) ? _.map(initData,field).join(', ') : _.get(initData,field); },
+            tune(data){ if(!_.isArray(data)) return data; let key = getHasKey(this.fetchKeys,_.keys(_.head(data))); return _.join(_.map(data,key),', ') },
             ...mapMutations('LIST',['setSelected']),
             clicked(){ this.setSelected({ list:this.dataListId, record:this.dataRecordId }); this.$el.getElementsByTagName('input')[0].checked = true; }
         }
+    }
+    function getHasKey(check,keys){
+        let ret = null;
+        _.forEach(check,(key) => { ret = _.includes(keys,key) ? key : null; if(!_.isNull(ret)) return false; });
+        return ret;
     }
 </script>
