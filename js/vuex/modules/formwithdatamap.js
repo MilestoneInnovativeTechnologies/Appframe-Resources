a03cc2b6538a7392b7132d642875d0be74abf477
Firose Hussain <github@firumon.com>
2019-01-22 12:23:26 +0530

    FormDataMap will now handle effectively

--- js/vuex/modules/formwithdatamap.js
+++ js/vuex/modules/formwithdatamap.js
@@ -1,30 +1,31 @@
 const state = {
     store: {},
     handler: {
-        'FormData': 'addMap',
+        'FormDataMap': 'addMap',
     }
 };
 
 const getters = {
-    map: ({ store }) => (form) => store[form]
+    map: ({ store }) => (form,data) => _.get(store,[form,data],null),
 };
 
 const actions = {
-    addMap({ state,commit },{ _response_data,FormData }){
-        _.forEach(FormData,(dObj,form) => {
-            if(_.isNil(state.store[form])) {
-                commit('addForm',{ form });
-                _.forEach(_response_data['Form'][form]['fields'],(field) => {
-                    commit('addField',{ form,field })
-                })
-            }
+    addMap({ state,commit },{ FormDataMap }){
+        _.forEach(FormDataMap,(mAry,form) => {
+            if(_.isNil(state.store[form])) commit('addForm',{ form });
+            let data = _.get(_.head(mAry),'resource_data',null); if(!data) return;
+            if(_.isNil(state.store[form][data])) commit('addFormData',{ form,data });
+            _.forEach(mAry,(fdmObj) => {
+                commit('addField',{ form,data,field:fdmObj.form_field,attribute:fdmObj.attribute,relation:fdmObj.relation })
+            })
         });
     }
 };
 
 const mutations = {
     addForm: ({ store },{ form }) => _.isNil(store[form]) ? Vue.set(store,form,{}) : true,
-    addField: ({ store },{ form,field }) => Vue.set(store[form],field.id,_.zipObject(['name','type','attribute','relation'],[field.name,field.type,field.name,undefined])),
+    addFormData: ({ store },{ form,data }) => _.isNil(store[form][data]) ? Vue.set(store[form],data,{}) : true,
+    addField: ({ store },{ form,data,field,attribute,relation }) => Vue.set(store[form][data],field,{ attribute,relation }),
 };
 
 export default {
