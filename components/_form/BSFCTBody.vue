cd65f459b7faa7021b1b8e19d36b805bfc27a517
Firose Hussain <github@firumon.com>
2019-01-23 13:07:26 +0530

    Select fields in collection form displays option text

--- components/_form/BSFCTBody.vue
+++ components/_form/BSFCTBody.vue
@@ -3,22 +3,25 @@
         <tr v-if="empty"><td :colspan="names.length+2" class="text-center">no data, start adding..</td></tr>
         <tr v-else v-for="(values,id,no) in collectiondata" :key="['FCD',this.form,this.name,id].join('-')">
             <th>{{ no+1 }}</th>
-            <td v-for="value in values">{{ value }}</td>
+            <td v-for="(value,name) in values">{{ displayText(name,value) }}</td>
             <th><BTN size="xs" type="outline-warning" @click.prevent="del(id)">Del</BTN></th>
         </tr>
     </tbody>
 </template>
 
 <script>
-    import { mapMutations } from 'vuex'
+    import { mapMutations,mapGetters } from 'vuex'
     export default {
         name: "BSFCTBody",
-        props: ['form','name','names','collectiondata'],
+        props: ['form','name','names','collectiondata','display_option'],
         computed: {
-            empty(){ return _.isEmpty(this.collectiondata) }
+            empty(){ return _.isEmpty(this.collectiondata) },
+            ...mapGetters('FOPT',{ options:'get' }),
         },
         methods: {
             ...mapMutations('FORM',['delCollectionValue','delAllCollectionValue']),
+            displayText(name,value){ return _.has(this.display_option,name) ? this.optionText(_.get(this.display_option,name),value) : value },
+            optionText(opt,key){ return _.get(this.options(opt),key,'-') },
             del(id){ this.delCollectionValue({ form:this.form,collection:this.name,id }) },
         },
         beforeDestroy(){
--- components/_form/BSFormCollection.vue
+++ components/_form/BSFormCollection.vue
@@ -4,7 +4,7 @@
         <div class="card-text">
             <div class="table-responsive"><table class="table table-sm">
                 <thead><tr><th>#</th><th v-for="(label,idx) in Labels" :key="['FC',form,'THC',idx+1].join('-')">{{ label }}</th><th></th></tr></thead>
-                <BSFCTBody :form="form" :name="name" :names="Names" :collectiondata="collectiondata"></BSFCTBody>
+                <BSFCTBody :form="form" :name="name" :names="Names" :collectiondata="collectiondata" :display_option="display_option"></BSFCTBody>
                 <BSFCTFoot :form="form" :name="name" :fields="Fields" :skip="skip" :count="Object.keys(collectiondata).length" :parent="parent" :key="tfkey"></BSFCTFoot>
             </table></div>
         </div>
@@ -26,7 +26,8 @@
             Names(){ return _.map(this.Fields,'name') },
             ...mapGetters({ getData:'collectiondata' }),
             collectiondata(){ return this.getData(this.form,this.name) },
-            tfkey(){ return _.last(_.keys(this.collectiondata)) }
+            tfkey(){ return _.last(_.keys(this.collectiondata)) },
+            display_option(){ return _(this.Fields).filter((v) => v.type === 'select').keyBy('name').mapValues('options.id').value() },
         }
     }
 </script>
