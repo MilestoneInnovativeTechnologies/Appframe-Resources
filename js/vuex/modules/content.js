b48a1b8286b0644db835280adbb23ea477734c17
Firose Hussain <github@firumon.com>
2019-01-24 21:51:18 +0530

    Current resource will also get stored

--- js/vuex/modules/content.js
+++ js/vuex/modules/content.js
@@ -1,16 +1,21 @@
 const state = {
     AppContent: null,
+    resource:null,
 };
 
 const actions = {
     beforeEachRoute({ state,commit },{ to }){
         if(to && to.meta && to.meta.target) commit('setContentAction',{ content:to.meta.target,action:to.params.action });
         commit('proceedRoute',null,{ root:true });
-    }
+    },
+    afterEachRoute({ rootGetters,commit },{ to }){
+        commit('setResource',rootGetters.actionResource(to.params.action));
+    },
 };
 
 const mutations = {
-    setContentAction(state,{ content,action }){ state[content] = action; }
+    setContentAction(state,{ content,action }){ state[content] = action; },
+    setResource(state,resource){ state.resource = resource; },
 };
 
 const getters = {
