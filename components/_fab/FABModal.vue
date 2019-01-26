a2986c4eee6388f741e8de5117380cf2e1039515
Firose Hussain <github@firumon.com>
2019-01-26 21:35:09 +0530

    New components for Float Action Button and Modal

---
+++ components/_fab/FABModal.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="modal fade" id="FABModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-dialog-overflow modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"> {{ title }} </h5>
+                </div>
+                <div class="modal-body">
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Click Me</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FABModal",
+        data(){
+            return {
+                title: 'Modal Title',
+                content: 'Modal Content',
+            }
+        },
+    }
+</script>
