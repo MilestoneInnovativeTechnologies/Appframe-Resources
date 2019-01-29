f972274d6c227fed59cfe199fb41ec2477a36dc4
Firose Hussain <github@firumon.com>
2019-01-29 20:02:15 +0530

    Icon margin can be controlled by assigning props

--- components/BTN.vue
+++ components/BTN.vue
@@ -1,5 +1,5 @@
 <template>
-    <button role="button" class="btn" :class="cls" v-on="listeners" :disabled="disabled"><icon class="mr-1" v-if="ipp">{{ icon }}</icon><slot></slot><icon class="ml-1" v-if="iap">{{ icon }}</icon></button>
+    <button role="button" class="btn" :class="cls" v-on="listeners" :disabled="disabled"><icon :class="ippcls" v-if="ipp">{{ icon }}</icon><slot></slot><icon :class="iapcls" v-if="iap">{{ icon }}</icon></button>
 </template>
 
 <script>
@@ -10,12 +10,15 @@
             size: { default: null },
             icon: { default: null },
             append: { default: false },
-            disabled: { default: false }
+            disabled: { default: false },
+            imargin: { default:1 }
         },
         computed: {
             cls(){ let cls = ['btn-'+this.type]; if(this.size) cls.push('btn-'+this.size); return cls; },
             iap(){ return this.icon && this.append !== false },
             ipp(){ return this.icon && this.append === false },
+            iapcls(){ return (this.iap) ? _.concat('ml',this.imargin).join('-') : '' },
+            ippcls(){ return (this.ipp) ? _.concat('mr',this.imargin).join('-') : '' },
             listeners(){ return this.$listeners }
         }
     }
