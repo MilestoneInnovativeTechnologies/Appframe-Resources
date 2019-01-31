38e2377a10449eadcb8d8207f8852293a234d51b
Firose Hussain <github@firumon.com>
2019-01-31 23:45:22 +0530

    Wait parameter added to make requests sequential

--- js/vuex/modules/server.js
+++ js/vuex/modules/server.js
@@ -1,16 +1,23 @@
 const state = {
     queue: [],
+    wait: false,
 };
 
 const actions = {
     post({ commit,dispatch },request){ commit('queue',request); dispatch('process'); },
-    process({ state,getters,dispatch,rootGetters }){ if(getters.queue && !rootGetters.pageLoading) server(state.queue.shift()).then(function({ data }){ dispatch('handleResponseData',data,{ root:true }) }); },
-    responseInterceptor({ dispatch }){ dispatch('process') }
+    responseInterceptor({ dispatch }){ dispatch('process') },
+    process({ state,getters,commit,dispatch,rootGetters }){
+        if(getters.queue && !rootGetters.pageLoading && state.wait === false){
+            server(state.queue.shift()).then(function({ data }){ dispatch('handleResponseData',data,{ root:true }) });
+            commit('wait',setTimeout(function({ dispatch,commit }){ commit('wait',false); dispatch('process') },100,{ dispatch,commit }))
+        }
+    }
 };
 
 const mutations = {
-    queue(state,request){ state.queue.push(request); },
-};
+        queue(state,request){ state.queue.push(request); },
+        wait(state,sec) { state.wait = sec },
+    };
 
 const getters = {
     queue(state){ return state.queue.length }
