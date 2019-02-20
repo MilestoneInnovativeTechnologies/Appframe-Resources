7b7260ca1a854dd451cf853df9459683b289897e
Firose Hussain <github@firumon.com>
2019-02-20 21:44:57 +0530

    Removes signature,pivot and timestamps from form data display

--- components/_formsubmit/FormSubmitDataSub.vue
+++ components/_formsubmit/FormSubmitDataSub.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card-body">
         <div class="card-title font-weight-bold"> {{ title }}</div>
-        <FormSubmitDataContent v-for="(desc,head,idx) in description" :title="head | read" :description="desc" :key="['fsd','sub','cnt',idx].join('-')" class="mx-5 border-top mb-0 pt-2"></FormSubmitDataContent>
+        <FormSubmitDataContent v-for="(desc,head,idx) in list" :title="head | read" :description="desc" :key="['fsd','sub','cnt',idx].join('-')" class="mx-5 border-top mb-0 pt-2"></FormSubmitDataContent>
     </div>
 </template>
 
@@ -9,6 +9,12 @@
     export default {
         name: "FormSubmitDataSub",
         props: ['title','description'],
+        data(){ return {
+            removeTitles: ['created_by','created_at','updated_by','updated_at','pivot'],
+        }},
+        computed: {
+            list(){ return _.omit(this.description,this.removeTitles) }
+        },
         filters: {
             read(text){ return _.upperCase(text.replace(/\W|\_/g,' ')); }
         }
