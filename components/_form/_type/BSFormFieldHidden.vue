072eb118d63a2f7aef7249ba7577c9feea080a2e
Firose Hussain <github@firumon.com>
2019-04-28 21:36:59 +0530

    Hidden field component added

---
+++ components/_form/_type/BSFormFieldHidden.vue
@@ -0,0 +1,14 @@
+<template>
+    <input type="hidden" :name="name" v-model.lazy="value" class="form-control" />
+</template>
+
+<script>
+    export default {
+        name: "BSFormFieldHidden",
+        props: ['name','dataFormId'],
+        mixins: [
+            require('./../../../js/common/BSFormFieldValueMixin').fieldValueMixin,
+            require('./../../../js/common/BSFormFieldDependMixin').fieldDependMixin
+        ]
+    }
+</script>
