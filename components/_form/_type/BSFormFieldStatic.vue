ba01044286e4e6f36ee2e5740bf9b0ec44897cf3
Firose <firumon@github.com>
2019-12-11 19:39:01 +0530

    Field type static created

---
+++ components/_form/_type/BSFormFieldStatic.vue
@@ -0,0 +1,14 @@
+<template>
+    <input type="text" readonly :value="value" class="form-control-plaintext" />
+</template>
+
+<script>
+    export default {
+        name: "BSFormFieldText",
+        props: ['type','name','dataFormId'],
+        mixins: [
+            require('./../../../js/common/BSFormFieldValueMixin').fieldValueMixin,
+            require('./../../../js/common/BSFormFieldDependMixin').fieldDependMixin
+        ]
+    }
+</script>
